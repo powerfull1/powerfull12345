e016d9d37bcfc739376b1f83eb37a8faae42da86
powerfull1 <pfull128@gmail.com>
2016-01-12 20:33:06 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,10 +230,10 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@yellow_hat [Founder]
+@yellow_hat [Developer]
+@yellow_hat [Developer]
+@yellow_hat [Manager]
 
 Special thanks to
 awkward_potato
